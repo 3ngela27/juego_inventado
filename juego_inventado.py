37d0eb02cc136a8aa3f21100cd0ec51833b3389b
3ngela27 <angeladiaz1@colegioguanenta.edu.co>
2024-03-15 09:14:38 -0500

    se agrego el codigo python del programa

---
+++ juego_inventado.py
@@ -0,0 +1,9 @@
+import random
+baraja = [1, 2, 3, 4, 5, 6, 7, 10, 11, 12]
+for i in range(3):
+     random.shuffle(baraja)
+     print(baraja)
+    
+[11, 3, 2, 6, 12, 10, 5, 4, 1, 7]
+[7, 3, 1, 10, 6, 12, 2, 11, 5, 4]
+[1, 11, 3, 2, 4, 12, 5, 10, 7, 6]
